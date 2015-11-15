21b54672e8f041f98e24de32b8edf870876a0f34
Chao <chaoguo@Chaos-MacBook-Air.local>
2015-11-15 01:12:47 -0800

    Added readme file

---
+++ README.m
@@ -0,0 +1,12 @@
+##Simple Twitter Sentiment Analysis
+
+#Goal
+*access the twitter Application Programming Interface(API) using python
+*estimate the public's perception (the sentiment) of a particular term or phrase
+*analyze the relationship between location and mood based on a sample of twitter data
+
+#Files
+*tweet_sentiment.py : calculate sentiment of each tweet based on a given dictionary
+*term_sentiment.py : use a certain algorithm to calculate sentiment of words that are not included in the given dictionary
+*frequency.py : calculate the frequency of hashtags
+*top_ten.py : find the top ten popular hashtags
